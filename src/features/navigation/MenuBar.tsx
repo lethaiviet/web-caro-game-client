a625bc75ce605ec535bce30a23b92436930c92d3
Viet Le <lethaiviet92@gmail.com>
2022-06-19 20:45:20 +0700

    + Add icon message with the count of notification

--- src/features/navigation/MenuBar.tsx
+++ src/features/navigation/MenuBar.tsx
@@ -1,25 +1,59 @@
 import {
   Navbar,
   Container,
-  Offcanvas,
   Nav,
   NavDropdown,
   Image,
+  Badge,
 } from "react-bootstrap";
 
 function DropdownToggleTitle() {
   return (
     <>
-      <a href="/">
+      <Image
+        style={{ width: "2.3rem" }}
+        src="https://via.placeholder.com/200?text=G"
+        alt="..."
+        fluid
+        roundedCircle
+      />
+      <span className="ms-2">YellowCat</span>
+    </>
+  );
+}
+
+function IconSetting() {
+  return (
+    <>
+      <Image
+        style={{ width: "1rem" }}
+        src="https://via.placeholder.com/200?text=G"
+        alt="..."
+        fluid
+        className="me-2"
+      />
+    </>
+  );
+}
+
+function MessageIcon() {
+  return (
+    <>
+      <div className="d-inline-flex position-relative me-2">
         <Image
-          style={{ width: "2.3rem" }}
+          style={{ width: "30px", height: "30px" }}
           src="https://via.placeholder.com/200?text=G"
           alt="..."
           fluid
-          roundedCircle
         />
-      </a>
-      <span className="ms-2">YellowCat</span>
+        <Badge
+          className="position-absolute top-0 start-100 translate-middle p-1 rounded-circle"
+          pill
+          bg="danger"
+        >
+          9+<span className="visually-hidden">unread messages</span>
+        </Badge>
+      </div>
     </>
   );
 }
@@ -30,24 +64,36 @@ function MenuBar() {
       <Navbar bg="light" expand="sm" className="mb-3">
         <Container fluid>
           <Navbar.Brand href="#">
-            <a href="/">
-              <Image
-                style={{ width: "2.3rem" }}
-                src="https://via.placeholder.com/200?text=G"
-                alt="..."
-                fluid
-              />
-            </a>
+            <Image
+              style={{ width: "2.3rem" }}
+              src="https://via.placeholder.com/200?text=G"
+              alt="..."
+              fluid
+            />
           </Navbar.Brand>
 
-          <Navbar.Toggle aria-controls="navbar" />
-          <Navbar.Collapse id="navbar" className="justify-content-end">
+          <Navbar.Toggle aria-controls="navbar-user-profile" />
+          <Navbar.Collapse
+            id="navbar-user-profile"
+            className="justify-content-end align-items-center"
+          >
+            <MessageIcon />
+
             <Nav>
               <NavDropdown id="nav-dropdown" title={<DropdownToggleTitle />}>
-                <NavDropdown.Item href="#action/3.1">Setting</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Profile</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.1">
+                  <IconSetting />
+                  Setting
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  <IconSetting />
+                  Profile
+                </NavDropdown.Item>
                 <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">Logout</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.4">
+                  <IconSetting />
+                  Logout
+                </NavDropdown.Item>
               </NavDropdown>
             </Nav>
           </Navbar.Collapse>
