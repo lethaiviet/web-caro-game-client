6427727b89401d8d0786da119a863cb1acdaa477
Viet Le <lethaiviet92@gmail.com>
2022-06-20 23:50:31 +0700

    + add react-custom-scrollbars

--- src/pages/chat/components/SideBarChat.tsx
+++ src/pages/chat/components/SideBarChat.tsx
@@ -1,16 +1,21 @@
-import { Offcanvas } from "react-bootstrap";
+//@ts-ignore
+import { Scrollbars } from "react-custom-scrollbars-2";
+
 import Contact from "./Contact";
 
 const data = Array.from({ length: 30 }, (i) => i);
 
 export default function SideBarChat() {
   return (
-    <Offcanvas show={true} className="mx-0" style={{ width: "282px" }}>
-      <Offcanvas.Body>
+    <div
+      className="position-relative top-0 start-0 bottom-0 vh-100 overflow-auto"
+      style={{ width: "250px" }}
+    >
+      <Scrollbars>
         {data.map((x) => (
           <Contact />
         ))}
-      </Offcanvas.Body>
-    </Offcanvas>
+      </Scrollbars>
+    </div>
   );
 }
