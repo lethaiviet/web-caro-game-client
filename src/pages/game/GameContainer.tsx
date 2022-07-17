79ac3d7eedc4f4682a64d5a1ef4859a2bc853418
Viet Le <lethaiviet92@gmail.com>
2022-07-17 17:14:10 +0700

    + Add game page template

--- src/pages/game/GameContainer.tsx
+++ src/pages/game/GameContainer.tsx
@@ -1,13 +1,26 @@
 import { getAvatarTemplate } from "@/utils/utils";
+import { useState } from "react";
 import { Card, Col, Container, Row, ProgressBar } from "react-bootstrap";
 import Scrollbars from "react-custom-scrollbars-2";
 import Avatar from "./components/Avatar";
+import variables from "./variables.module.scss";
 
-const data = Array.from({ length: 10 * 20 }, (i) => i);
+const initEmptyBoardGameData = () => {
+  const numRows = parseInt(variables["num-of-rows-grid"]);
+  const numCols = parseInt(variables["num-of-cols-grid"]);
+  let gridData = [];
+  for (let r = 0; r < numRows; r++) {
+    gridData[r] = Array(numCols).fill("");
+  }
+  return gridData;
+};
+
 export default function GameContainer() {
+  const [boardGameData, setBoardGameData] = useState(initEmptyBoardGameData());
+
   return (
-    <Container>
-      <Row className="d-flex align-items-center mb-3">
+    <Container className="vh-100">
+      <Row className="d-flex align-items-center my-2">
         <Col lg={3} md={2} sm={2} xs={2}>
           <Card border="dark" className="p-2" style={{ borderRadius: "10px" }}>
             <Row className="d-flex justify-content-start align-items-center">
@@ -51,6 +64,7 @@ export default function GameContainer() {
 
       <div className="scroll-bar-board-game">
         <Scrollbars
+          autoHide={true}
           renderThumbVertical={(props) => (
             <div {...props} className="thumb-vertical" />
           )}
@@ -64,10 +78,29 @@ export default function GameContainer() {
             <div {...props} className="track-horizontal" />
           )}
         >
-          <div className="board-game">
-            {data.map((x) => (
-              <div className="cell">x</div>
-            ))}
+          <div className="board-game-10x20">
+            {boardGameData.map((rowData) =>
+              rowData.map((cell) => {
+                if (cell === "o") {
+                  return (
+                    <div className="cell">
+                      <div className="O-symbol center" />
+                    </div>
+                  );
+                }
+
+                if (cell === "x") {
+                  return (
+                    <div className="cell">
+                      <div className="X-symbol center" />
+                      <div className="X-symbol center" />
+                    </div>
+                  );
+                }
+
+                return <div className="cell" />;
+              })
+            )}
           </div>
         </Scrollbars>
       </div>
